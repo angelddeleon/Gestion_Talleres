866b37f5478a4c624be2d6096f446679210598fe
Andres Ng <ngandres28@gmail.com>
2024-11-16 17:57:35 -0400

    Add gets endpoints from reparaciones

--- backend/routes/reparaciones/reparacionesRoutes.js
+++ backend/routes/reparaciones/reparacionesRoutes.js
@@ -1,6 +1,34 @@
 import express from "express"
 import { Router } from "express"
+import client from "./model.js"
 
-export const reparacionesRouter = Router()
+const reparacionesRouter = Router()
+
+
+
+reparacionesRouter.get("/", async (req, res) =>{
+
+    try{
+        const reparaciones = await client.execute(`SELECT * FROM REPARACIONES`)
+        return res.json(reparaciones.rows)
+
+    }catch{
+        return []
+
+    }
+})
+
+
+reparacionesRouter.get("/detalles", async (req, res) =>{
+    try{
+        const detalles = await client.execute(`SELECT * FROM DETALLES_REPARACIONES`)
+        return res.json(detalles.rows)
+
+    }catch{
+        return []
+    }
+})
+
+
 
 export default reparacionesRouter
